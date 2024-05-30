d78eb6839e732cb3ce7a041724a09be7950c55d3
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-30 23:50:07 +0900

    Test

--- lib/components/lists/member_card_list.dart
+++ lib/components/lists/member_card_list.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 
 class MemberCardList extends StatefulWidget {
   const MemberCardList({super.key});
@@ -12,27 +12,39 @@ class _MemberCardListState extends State<MemberCardList> {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      options: CarouselOptions(height: 350.0),
+      options: CarouselOptions(
+        height: 250.0,
+        autoPlay: true,
+        viewportFraction: 0.65,
+        autoPlayInterval: const Duration(seconds: 4),
+        autoPlayAnimationDuration: const Duration(milliseconds: 4000),
+      ),
       items: [1, 2, 3, 4, 5].map((i) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                width: 250,
                 decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.surface,
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  borderRadius: const BorderRadius.all(Radius.circular(7)),
                 ),
                 child: Center(
-                  child: Column(
-                    children: [
-                      Image.network(
-                        'https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter',
-                        width: 300,
-                        height: 300,
-                      ),
-                      Text('test$i'),
-                    ],
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          'https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter',
+                          width: 200,
+                          height: 200,
+                        ),
+                        Text(
+                          'test$i',
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
                 ));
           },
