ba6457ba4cb0db9fbeeec2fd8d5c19f9941b14c6
Lee Seung Jin <dltmdwls154@gmail.com>
2024-10-29 23:56:32 +0900

    Test

---
+++ lib/assets/text_data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class TextStyles {
+  static const h1 = TextStyle(
+    fontSize: 44,
+    height: 1,
+  );
+  static const h2 = TextStyle(fontSize: 36);
+  static const h3 = TextStyle(fontSize: 28);
+  static const h4 = TextStyle(fontSize: 24);
+  static const h5 = TextStyle(fontSize: 20);
+  static const l1 = TextStyle(fontSize: 20);
+  static const l2 = TextStyle(fontSize: 20);
+  static const m1 = TextStyle(fontSize: 18);
+  static const m2 = TextStyle(fontSize: 18);
+  static const n1 = TextStyle(fontSize: 16);
+  static const n2 = TextStyle(fontSize: 16);
+  static const s1 = TextStyle(fontSize: 14);
+  static const s2 = TextStyle(fontSize: 14);
+  static const xs1 = TextStyle(fontSize: 12);
+  static const xs2 = TextStyle(fontSize: 12);
+}
