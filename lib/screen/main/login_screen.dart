fe05cd7bc61ddb920e5fd72472b2a38babb9f96b
Lee Seung Jin <dltmdwls154@gmail.com>
2025-01-22 23:14:46 +0900

    Test

--- lib/screen/main/login_screen.dart
+++ lib/screen/main/login_screen.dart
@@ -7,7 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Login Screen'),
+        child: Text('Sign In Screen'),
       ),
     );
   }
